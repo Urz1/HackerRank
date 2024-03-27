3c564192e09f5020b672709ca33b69523c7f92ac
Sadam Husen Ali <101430982+Urz1@users.noreply.github.com>
2024-03-28 02:29:15 +0300

    Set .difference() Operation.py

---
+++ Set .difference() Operation.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+English = int(input())
+studentE = set(map(int,input().split()))
+French = int(input())
+studentF = set(map(int,input().split()))
+print(len(studentE.difference(studentF)))
